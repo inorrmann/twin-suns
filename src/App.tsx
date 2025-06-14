053bf9d7804e8327f4b295ecb70b27ba1ff751b1
Ingrid Norrmann-Vigil <inorrmann@gmail.com>
2025-06-13 23:53:15 -0700

    removed not found

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Link } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
-import NotFound from './pages/NotFound'
+// import NotFound from './pages/NotFound'
 import './App.css'
 
 export default function App() {
   return (
     <>
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/about">About</Link>
+      </nav>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/about" element={<About/>}/>
-        <Route path="*" element={<NotFound/>}/>
+        {/*<Route path="*" element={<NotFound/>}/>*/}
       </Routes>
     </>
   )
