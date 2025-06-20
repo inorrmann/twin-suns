256482cb1d9a8af5877de2ab990226207bed6b72
Ingrid Norrmann-Vigil <inorrmann@gmail.com>
2025-06-19 21:02:27 -0700

    mission 2 complete

--- src/components/Missions/06282025/M2.tsx
+++ src/components/Missions/06282025/M2.tsx
@@ -1,19 +1,98 @@
-export default function M2()
+import { useState } from "react";
+import bgImage from '../../../assets/screen.jpg';
+
+export default function M1()
 {
+  const [showClue, setShowClue] = useState(false);
+
+  function handleShowClue(){
+    setShowClue(true);
+  }
+
   return (
-    <>
-      <p className="text-3xl font-bold">Droid Diagnostic — Advanced AI Corruption</p>
-      <br/>
-      <p className="text-2xl">
-        An assassin droid unit has begun reciting corrupted data strings.
-        Your job is to identify which memory core has been compromised.
-      </p>
-      <br/>
-      <ul className="text-2xl font-bold">
-        <li>Primary protocol: 1011100100010110</li>
-        <li>Secondary protocol: 1011100100010110</li>
-        <li>Tertiary protocol: 1011100101110110</li>
-      </ul>
-    </>
+    <div className="bg-cover bg-center bg-fixed min-h-screen text-green-200 text-xl"
+         style={{ backgroundImage: `url(${bgImage})` }}
+    >
+      <div className="p-10 sm:p-20 sm:pb-10 ">
+        <p className="text-3xl font-bold">Sith Holocron: Trial of Intent</p>
+        <br/>
+        <div className="bg-white/10 px-2">
+          <p className="text-left">
+            <b>ARTIFACT ID:</b> SITH-HOLO-XI/RED-OBSIDIAN <br/>
+            <b>CLASSIFICATION:</b> Ancient Sith Teaching Core – Locked Mode <br/>
+            <b>STATUS:</b> Active – User Detected <br/>
+          </p>
+        </div>
+        <p>
+          Only those who understand the nature of power may proceed.
+        </p>
+        <br/>
+        <p className="font-bold">
+          [ GATEKEEPER RESPONSE ACTIVE… ]
+        </p>
+        <div className="bg-white/10 px-2">
+          <p className="text-left">
+            “The strong must dominate.<br/>
+            The weak must be ruled.<br/>
+            But power gained too quickly decays from within.<br/>
+            What, then, is the true path of the Sith?”
+          </p>
+        </div>
+        <br/>
+
+        <p className="font-bold">
+          [ RESPONSE OPTIONS PRESENTED IN SITH SCRIPT: ]
+        </p>
+        <div className="bg-white/10 px-2">
+          <p className="text-left">
+            A. Mercy is strength. <br/>
+            B. Anger without purpose is chaos. <br/>
+            C. Power through patience. <br/>
+            D. Shadows must remain shadows.
+          </p>
+        </div>
+        <br/>
+
+        <p>
+          Your task is to reflect on the Sith Code and philosophy,
+          and choose the phrase that aligns with Sith teachings
+        </p>
+        <br/>
+
+        {!showClue &&
+          <>
+            <button
+              type="button"
+              className="bg-green-950 hover:bg-black "
+              onClick={handleShowClue as never}
+            >
+              Click for Clue
+            </button>
+            <br/>
+          </>
+        }
+
+        {showClue &&
+          <>
+            <p className="text-left font-bold">
+
+            </p>
+            <p className="text-left px-4">
+              Shows mastery over impulsive emotion.
+            </p>
+            <br/>
+          </>
+        }
+
+        <br/>
+        <button
+          type="button"
+          className="bg-black hover:bg-black"
+          onClick={() => window.open('https://www.google.com', '_blank', 'noopener, noreferrer')}
+        >
+          Submit Response
+        </button>
+      </div>
+    </div>
   );
 }
