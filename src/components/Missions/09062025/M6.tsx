555faaf9653fc36845524d666f1c7ef7334af8c3
Ingrid Norrmann-Vigil <inorrmann@gmail.com>
2025-08-28 21:57:34 -0700

    templates fixed

--- src/components/Missions/09062025/M6.tsx
+++ src/components/Missions/09062025/M6.tsx
@@ -3,11 +3,19 @@ import bgImage from '../../../assets/screen.jpg';
 
 export default function M6()
 {
-  const [showClue1, setShowClue1] = useState(false);
+  const [input, setInput] = useState('');
+  const [authenticated, setAuthenticated] = useState(false);
+  const [incorrectPassword, setIncorrectPassword] = useState(false);
 
-  function handleShowClue1(){
-    setShowClue1(true);
-  }
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (input.toLowerCase().trim() == 'twin suns 77') {
+      setAuthenticated(true);
+      setIncorrectPassword(false);
+    } else {
+      setIncorrectPassword(true);
+    }
+  };
 
   return (
     <div className="bg-cover bg-center bg-fixed min-h-screen text-green-200 text-xl"
@@ -16,63 +24,88 @@ export default function M6()
       <div className="p-10 sm:p-20 sm:pb-10 ">
         <p className="text-3xl font-bold">TITLE GOES HERE</p>
         <br/>
-        <p>
 
-        </p>
-        <br/>
-        <p className="font-bold">
+        {!authenticated &&
+          <div className="bg-white/10 px-2">
+            <p className="text-center">
+              <b> ENTER XXX TO CONTINUE
+              </b>
+              <br/>
+              <br/>
+            </p>
+            <form
+              onSubmit={handleSubmit}
+              className="pb-2 text-green-800"
+            >
+              <input
+                type="password"
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+                className="text-xl bg-gray-700 p-2 max-w-64"
+              />
+              <button
+                type="submit"
+                className="ml-4 bg-black text-white pb-2"
+              >Submit
+              </button>
+            </form>
+          </div>
+        }
 
-        </p>
-        <div className="bg-white/10 px-2">
-          <p className="text-left">
-            <b></b> SCREEN WITH TRANSPARENT BACKGROUND <br/>
+        {incorrectPassword &&
+          <div>
+            <br/>
+            <p className="text-2xl warning">
+              INCORRECT PASSWORD
+            </p>
+            <br/>
+          </div>
+        }
 
-          </p>
-        </div>
-        <br/>
+        {authenticated &&
+          <div>
+            <br/>
+            <p className="text-left">
 
-        <p className="text-left">
+            </p>
+            <br/>
+            <p className="text-left">
 
-        </p>
-        <br/>
-        <p className="text-left">
+            </p>
+            <br/>
+            <p className="text-left">
 
-        </p>
-        <br/>
+            </p>
+            <br/>
+            <p className="text-left">
 
-        {!showClue1 &&
-          <>
+            </p>
+            <br/>
+            <p className="text-left">
+
+            </p>
+            <br/>
+            <br/>
+
             <button
               type="button"
-              className="bg-green-950 hover:bg-black "
-              onClick={handleShowClue1 as never}
+              className="bg-black hover:bg-black"
+              onClick={() => window.open('https://cerosoul.github.io/imperialpatternanalysis/', '_blank', 'noopener, noreferrer')}
             >
-              Click for Clues
+              Click here to
             </button>
-            <br/>
-          </>
+          </div>
         }
 
-        {showClue1 &&
-          <>
-            <p className="text-left font-bold">
 
-            </p>
-            <p className="text-left px-4">
+        {/*<div className="bg-white/10 px-2">*/}
+        {/*  <p className="text-left">*/}
+        {/*    <b></b> SCREEN WITH TRANSPARENT BACKGROUND <br/>*/}
 
-            </p>
-            <br/>
-          </>
-        }
+        {/*  </p>*/}
+        {/*</div>*/}
+        {/*<br/>*/}
 
-        <br/>
-        <button
-          type="button"
-          className="bg-black hover:bg-black"
-          onClick={() => window.open('https://www.google.com', '_blank', 'noopener, noreferrer')}
-        >
-          Submit Location
-        </button>
       </div>
     </div>
   );
