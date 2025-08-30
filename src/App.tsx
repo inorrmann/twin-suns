991b3444f674e2ecf7e33d3676a02d5ef979dfb1
Ingrid Norrmann-Vigil <inorrmann@gmail.com>
2025-08-30 11:26:52 -0700

    routes improved

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
 import { Routes, Route } from 'react-router-dom'
 import About from './pages/about.tsx'
 import Home from './pages/home.tsx'
-// import Mission from "./pages/scavenger-hunt/mission.tsx";
+import Mission from "./pages/scavenger-hunt/mission.tsx";
 import MissionSept from "./pages/scavenger-hunt/mission-sept-25.tsx";
 import './App.css'
 
 export default function App() {
-  // https://www.twinsunsranch.com/#/scavenger-hunt/06282025/mission/2
+  // https://www.twinsunsranch.com/#/scavenger-hunt/06282025/2
 
   return (
     <>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/about" element={<About/>}/>
-        {/*<Route path="/scavenger-hunt/06282025/mission/:id" element={<Mission/>}/>*/}
-        <Route path="/scavenger-hunt/09062025/mission/:id" element={<MissionSept/>}/>
+        <Route path="/scavenger-hunt/06282025/:id" element={<Mission/>}/>
+        <Route path="/scavenger-hunt/09062025/:id" element={<MissionSept/>}/>
       </Routes>
     </>
   )
