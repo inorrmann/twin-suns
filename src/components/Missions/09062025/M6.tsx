707af882f93ced4f7d15551e543105db11adeea7
Ingrid Norrmann-Vigil <inorrmann@gmail.com>
2025-08-29 23:35:23 -0700

    Mission 6

--- src/components/Missions/09062025/M6.tsx
+++ src/components/Missions/09062025/M6.tsx
@@ -9,7 +9,7 @@ export default function M6()
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    if (input.toLowerCase().trim() == 'twin suns 77') {
+    if (input.toLowerCase().trim() == 'angletimer') {
       setAuthenticated(true);
       setIncorrectPassword(false);
     } else {
@@ -22,13 +22,18 @@ export default function M6()
          style={{ backgroundImage: `url(${bgImage})` }}
     >
       <div className="p-10 sm:p-20 sm:pb-10 ">
-        <p className="text-3xl font-bold">TITLE GOES HERE</p>
+        <p className="text-3xl font-bold">ARRIVAL ON SALLAK</p>
+        <br/>
+        <br/>
+        <p className="font-bold text-2xl">
+          THE JOURNAL
+        </p>
         <br/>
 
         {!authenticated &&
           <div className="bg-white/10 px-2">
             <p className="text-center">
-              <b> ENTER XXX TO CONTINUE
+              <b> ENTER THE PASSWORD FROM ASTROMECH SENSOR ARRAY TO CONTINUE
               </b>
               <br/>
               <br/>
@@ -66,23 +71,31 @@ export default function M6()
           <div>
             <br/>
             <p className="text-left">
-
+              The tunnels were colder than expected, the air thick with the weight of something ancient.
+              It was there, in a shadowed alcove of stone, that they found it: a blackened pack, brittle with age,
+              wedged between broken slabs. Inside was a journal sealed with a strange mechanism.
             </p>
             <br/>
             <p className="text-left">
-
+              When they cracked it open, the pages seemed to resist their eyes. The letters twisted, shapes shifting
+              unnaturally as though alive. One settler muttered the word that chilled the group to silence: Sith.
             </p>
             <br/>
             <p className="text-left">
-
+              The journal was more than notes — it was a warning, encoded through a cipher meant to hide its meaning.
+              To reveal the message, the settlers would need to shift the letters into clarity. With every fragment
+              decoded, a grim picture began to take shape: explorers who had come before, seduced or destroyed
+              by a darkness that lingered on Sallak.
             </p>
             <br/>
             <p className="text-left">
-
+              Whatever had been written here, it was meant to be hidden. And yet, the settlers knew they had no
+              choice but to read it.
             </p>
             <br/>
             <p className="text-left">
-
+              Decipher the decoded phrase leading to the Cipher Vault. When you finish, you will receive a password.
+              Remember that password to unlock the next portion of the story, located near the homestead.
             </p>
             <br/>
             <br/>
@@ -90,22 +103,12 @@ export default function M6()
             <button
               type="button"
               className="bg-black hover:bg-black"
-              onClick={() => window.open('https://cerosoul.github.io/imperialpatternanalysis/', '_blank', 'noopener, noreferrer')}
+              onClick={() => window.open('https://cerosoul.github.io/sithcypherchallenge/', '_blank', 'noopener, noreferrer')}
             >
-              Click here to
+              Click here to decipher the phrase
             </button>
           </div>
         }
-
-
-        {/*<div className="bg-white/10 px-2">*/}
-        {/*  <p className="text-left">*/}
-        {/*    <b></b> SCREEN WITH TRANSPARENT BACKGROUND <br/>*/}
-
-        {/*  </p>*/}
-        {/*</div>*/}
-        {/*<br/>*/}
-
       </div>
     </div>
   );
